6bd0e1e2cdf5ffab2b30b45ffe06ef31a0c967e3
fl0xy <florian@deeken.net>
2021-10-04 15:44:31 +0200

    added MAC, POW, MAM

---
+++ RCAS/POW/POWChart.gd
@@ -0,0 +1,17 @@
+tool
+extends Node2D
+
+const MAX_POWER = 11
+const MAX_HIGHT = 46
+const WIDTH = 11
+
+export(float) var inPower setget changeinPower
+
+func _ready():
+	pass # Replace with function body.
+
+
+func changeinPower(newInPower):
+	inPower = newInPower
+	$bar.scale = Vector2(WIDTH, clamp(-(float(MAX_HIGHT)/11*inPower), -MAX_HIGHT, 0))
+	
