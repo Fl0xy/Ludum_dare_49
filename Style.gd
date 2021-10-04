b0094aea59ff92b8941e64dfc723403b77d5eedf
fl0xy <florian@deeken.net>
2021-10-04 11:09:13 +0200

    more work on device, added global style, made device sprites white

---
+++ Style.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+const ANTIMATTER_COLOR: Color = Color("#fbf236")
+const MATTER_COLOR: Color = Color("#df7126")
+const ENERGY_COLOR: Color = Color("#5b6ee1")
+const ON_COLOR: Color = Color("#99e550")
+const OFF_COLOR: Color = Color("#4b692f")
+const BLUE_COLOR: Color = Color("#5b6ee1")
+const DARKBLUE_COLOR: Color = Color("#222034")
