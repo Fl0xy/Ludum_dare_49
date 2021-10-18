8a7e66d3852e813712a3aa98ad3a11cfd96901a4
fl0xy <florian@deeken.net>
2021-10-18 19:49:01 +0200

    added Device discrage indicator

--- Style.gd
+++ Style.gd
@@ -2,8 +2,11 @@ extends Node
 
 
 const ANTIMATTER_COLOR: Color = Color("#fbf236")
+const ANTIMATTER_COLOR_DARK: Color = Color("#a7a002")
 const MATTER_COLOR: Color = Color("#df7126")
+const MATTER_COLOR_DARK: Color = Color("#a44605")
 const ENERGY_COLOR: Color = Color("#5b6ee1")
+const ENERGY_COLOR_DARK: Color = Color("3147cf")
 const ON_COLOR: Color = Color("#99e550")
 const OFF_COLOR: Color = Color("#4b692f")
 const BLUE_COLOR: Color = Color("#5b6ee1")
